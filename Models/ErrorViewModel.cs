64e9c4586121c38fec64fb6b100fe67756264cf5
thaibao0225 <thaibao0225@gmail.com>
2021-05-06 14:13:49 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_ATN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
